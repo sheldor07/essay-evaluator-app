b9ba9c5222c10fca1abce917016a24596ebd7e7c
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-07-17 22:09:07 +0530

    feat added hugging face api for bart-mnli model && style added table for showing confidence labels
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,93 @@
+"use client";
+import { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default async function Home() {
-  const user = await currentUser();
+export default function Home() {
+  const { user } = useUser();
+  const [text, setText] = useState("");
+  const [predictions, setPredictions] = useState({});
+  const candidateLabels = ["technology", "humanities", "sciences", "business"];
+  const [loading, setLoading] = useState(false);
   if (!user) return <div>Not logged in</div>;
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = setTimeout(() => {
-      return "this is the response";
-    }, 2000);
-    console.log(response);
-    // const data = await response.json();
-    // console.log(data);
-  }
+    setLoading(true);
+    const data = {
+      inputs: text,
+      parameters: { candidate_labels: candidateLabels },
+    };
+
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/facebook/bart-large-mnli",
+      {
+        headers: {
+          Authorization: "Bearer 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",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+
+    const result = await response.json();
+    const newPredictions = {};
+    for (let i = 0; i < result.labels.length; i++) {
+      newPredictions[result.labels[i]] = result.scores[i];
+    }
+    setPredictions(newPredictions);
+    setLoading(false);
+  };
 
   return (
-    <div className="bg-primary text-primary-foreground">
-      {" "}
+    <div className=" text-primary-foreground">
       <nav className="flex flex-wrap items-center justify-between p-6 bg-zinc-500">
         <div className="flex items-center flex-shrink-0 mr-6 text-white">
           <span className="text-xl font-semibold tracking-tight">
             Welcome to Essay Evaluator!
           </span>
         </div>
-
         <UserButton afterSignOutUrl="/" />
       </nav>
       <div className="grid grid-cols-2 m-12">
         <div className="flex flex-col">
-
-          <form
-         >
-            <Textarea className="h-72" />
+          <form onSubmit={handleSubmit}>
+            <Textarea
+              className="h-72"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
             <Button className="my-6 bg-primary " variant="outline">
               Submit
             </Button>
           </form>
         </div>
+        {loading ? (
+          <>
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </>
+        ) : (
+          <div className="flex flex-col p-4 mt-6 rounded-xl border-slate-800">
+            <div className="flex border border-slate-600">
+              <div className="w-1/2 px-4 py-2 border-r border-slate-600">
+                Label
+              </div>
+              <div className="w-1/2 px-4 py-2">Score</div>
+            </div>
+            {Object.entries(predictions).map(([label, score]) => (
+              <div key={label} className="flex border-b border-slate-600">
+                <div className="w-1/2 px-4 py-2 border-r border-slate-600">
+                  {label}
+                </div>
+                <div className="w-1/2 px-4 py-2">{score}</div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
