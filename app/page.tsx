3b7c5c3dccec3ae4c277d0fd3f1c7ef176dbee2d
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-07-18 00:35:44 +0530

    feat added multiple model support for topic classification
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -14,34 +14,52 @@ export default function Home() {
   const { user } = useUser();
   const [text, setText] = useState("");
   const [predictions, setPredictions] = useState<Record<string, number>>({});
+  const [model, setModel] = useState("");
+
   const candidateLabels = ["technology", "humanities", "sciences", "business"];
   const [loading, setLoading] = useState(false);
   if (!user) return <div>Not logged in</div>;
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleSubmit = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     event.preventDefault();
     setLoading(true);
     const data = {
       inputs: text,
       parameters: { candidate_labels: candidateLabels },
     };
+    let modelURL = "";
 
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/facebook/bart-large-mnli",
-      {
-        headers: {
-          Authorization: "Bearer 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",
-        },
-        method: "POST",
-        body: JSON.stringify(data),
-      }
-    );
+    switch (model) {
+      case "mnli":
+        modelURL =
+          "https://api-inference.huggingface.co/models/facebook/bart-large-mnli";
+        break;
+      case "deberta":
+        modelURL =
+          "https://api-inference.huggingface.co/models/MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli";
+        break;
+      // Add more cases as needed
+      default:
+        modelURL =
+          "https://api-inference.huggingface.co/models/facebook/bart-large-mnli";
+        break;
+    }
+    const response = await fetch(modelURL, {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUGGINGFACE_API_KEY}`,
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
 
     const result = (await response.json()) as IApiResponse;
     let newPredictions: Record<string, number> = {};
     for (let i = 0; i < result.labels.length; i++) {
       newPredictions[result.labels[i]] = result.scores[i];
     }
+    console.log(newPredictions);
     setPredictions(newPredictions);
     setLoading(false);
   };
@@ -58,16 +76,37 @@ export default function Home() {
       </nav>
       <div className="grid grid-cols-2 m-12">
         <div className="flex flex-col">
-          <form onSubmit={handleSubmit}>
-            <Textarea
-              className="h-72"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
-            <Button className="my-6 bg-primary " variant="outline">
-              Submit
+          <Textarea value={text} onChange={(e) => setText(e.target.value)} />
+          <div className="flex flex-col w-full p-2">
+            <p className="font-bold text-black text-md">Topic Classification</p>
+            <Button
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                setModel("mnli");
+                handleSubmit(e);
+              }}
+              className="w-full my-1 bg-teal-200"
+              variant="outline">
+              facebook/bart-large-mnli
             </Button>
-          </form>
+            <Button
+              onClick={(e) => {
+                setModel("deberta");
+                handleSubmit(e);
+              }}
+              className="w-full my-1 bg-teal-200"
+              variant="outline">
+              MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli
+            </Button>
+            {/* <Button
+              onClick={(e) => {
+                setModel("mnli");
+                handleSubmit(e);
+              }}
+              className="w-full my-1 bg-teal-200"
+              variant="outline">
+              Submit
+            </Button> */}
+          </div>
         </div>
         {loading ? (
           <>
@@ -76,6 +115,7 @@ export default function Home() {
           </>
         ) : (
           <div className="flex flex-col p-4 mt-6 rounded-xl border-slate-800">
+            <h1 className="text-xl text-black ">{model}</h1>
             <div className="flex border border-slate-600">
               <div className="w-1/2 px-4 py-2 border-r border-slate-600">
                 Label
